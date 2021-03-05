583e92335a298d3c25306c52d4ad961ecf2e0dfd
slil_puangpoom <slil.puangpoom@gmail.com>
2021-03-05 15:44:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [response,setResponse] = useState('');
+
+  const callApi = async () => {
+    setResponse('loading...');
+    const api = await axios.get('https://1hy62txsdl.execute-api.ap-southeast-1.amazonaws.com/blink-aws-class-public/awsclass');
+    setResponse(api.data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={callApi}>Click to call api</button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {response}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
